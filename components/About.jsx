42ce84a9c010bedba63e945938f1d8c442f72a39
Smeet Agrawal <smeetagrawal.886@gmail.com>
2023-01-25 00:17:53 +0530

    fix small image url issue

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import AboutImg from "../public/assets/about.jpg";
+import AboutImg from "/public/assets/about.jpg";
 
 function About() {
   return (
