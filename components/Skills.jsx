874c37f65375a48ac374d9d698e62eed4d3ba62f
Smeet Agrawal <smeetagrawal.886@gmail.com>
2023-02-12 19:20:22 +0530

    Updated skill component content

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import tailwindLogo from "/public/assets/skills/tailwind.png";
 import reactLogo from "/public/assets/skills/react.png";
 import javascriptLogo from "/public/assets/skills/javascript.png";
-import firebaseLogo from "/public/assets/skills/firebase.png";
 import htmlLogo from "/public/assets/skills/html.png";
 import cssLogo from "/public/assets/skills/css.png";
 import githubLogo from "/public/assets/skills/github1.png";
-import shopifyLogo from "/public/assets/skills/shopify.png";
+import cicdLogo from "/public/assets/skills/CI_CD.png";
+import nodejsLogo from "/public/assets/skills/node.png";
 
 function Skills() {
   return (
@@ -21,7 +21,7 @@ function Skills() {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={htmlLogo} width={64} height={64} alt="Html Image" />
+                <Image src={htmlLogo} width={64} height={64} alt="Html" />
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>HTML</h3>
@@ -31,7 +31,7 @@ function Skills() {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image src={cssLogo} width={64} height={64} alt="Html Image" />
+                <Image src={cssLogo} width={64} height={64} alt="CSS" />
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>CSS</h3>
@@ -45,7 +45,7 @@ function Skills() {
                   src={javascriptLogo}
                   width={64}
                   height={64}
-                  alt="Html Image"
+                  alt="Javascript"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
@@ -56,12 +56,7 @@ function Skills() {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image
-                  src={reactLogo}
-                  width={64}
-                  height={64}
-                  alt="Html Image"
-                />
+                <Image src={reactLogo} width={64} height={64} alt="React" />
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>React</h3>
@@ -72,29 +67,24 @@ function Skills() {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src={tailwindLogo}
+                  src={reactLogo}
                   width={64}
                   height={64}
-                  alt="Html Image"
+                  alt="React Native"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind</h3>
+                <h3>React Native</h3>
               </div>
             </div>
           </div>
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image
-                  src={firebaseLogo}
-                  width={64}
-                  height={64}
-                  alt="Html Image"
-                />
+                <Image src={cicdLogo} width={64} height={64} alt="CI CD" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>Firebase</h3>
+                <h3>CI / CD</h3>
               </div>
             </div>
           </div>
@@ -113,18 +103,14 @@ function Skills() {
               </div>
             </div>
           </div>
+
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image
-                  src={shopifyLogo}
-                  width={64}
-                  height={64}
-                  alt="Html Image"
-                />
+                <Image src={nodejsLogo} width={64} height={64} alt="Nodejs" />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3>Shopify</h3>
+                <h3>Node.js</h3>
               </div>
             </div>
           </div>
